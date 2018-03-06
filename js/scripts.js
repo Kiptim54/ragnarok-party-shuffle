ed52d0f94434622a6e08d35f94ed0be09b45cfe2
Brenda Kiptim <kiptim54@gmail.com>
2018-03-06 15:14:27 +0300

    add js for play and pause icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
-$("#play").click(function(){
-$("#play").hide();
-$("#pause").show();
-});
-$("#pause").click(function(){
-  $("#pause").hide();
-  $("#play").show();
-})
+  /*hide the audio tag*/
+  /*$("audio").hide();*/
+  /*swtich between play and pause*/
+
+    audioplayer();
+  $("#play").click(function(){
+    $("#play").hide();
+    $("#pause").show();
+  });
+  $("#pause").click(function(){
+    $("#pause").hide();
+    $("#play").show();
+  });
+  /*connect the icon play and audio play*/
+  $("#play").click(function(){
+    $("audio").trigger("play");
+  });
+  $("#pause").click(function(){
+    $("audio").trigger("pause");
+  });
+
+skip();
+
+  //Business Logic
+  function audioplayer(){
+    $("#musicplayer")[0].src=$("#playlist li a")[0];
+    $("#playlist li a").click(function(event){
+      event.preventDefault();
+    });
+  };
+  function skip(){
+    $("#skipforward").click(function(){
+      $("#musicplayer")[0].src=$("#playlist li a")["#playlist li a".length+1];
+      $("#musicplayer")[0].play();
+      $("#pause").show();
+
+    });
+  };
 });
