ab436f1b213e662eb3e908996e0395317b974cc3
Brenda Kiptim <kiptim54@gmail.com>
2018-03-06 22:23:23 +0300

    add particles.js

--- js/scripts.js
+++ js/scripts.js
@@ -50,6 +50,9 @@ $(document).ready(function(){
         $("#pause").show();
       });
       };
-
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
 
     });
