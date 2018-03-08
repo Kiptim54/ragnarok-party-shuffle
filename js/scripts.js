28d290d955ef958035490c7e92384aa6f5481681
Brenda Kiptim <kiptim54@gmail.com>
2018-03-08 14:53:22 +0300

    add ogg music files

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,50 @@
 //User Logic
+
+  $("#btn").click(function(){
+  $("music-shuffle.html").addclass("animated slideIn");
+  });
 //==================================================================================================================
+//This will make text appear as a fade in.
+var $el = $(".slideText1:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*100).fadeIn(200);
+});
+var $el = $(".slideText2:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*400).fadeIn(500);
+});
+var $el = $(".slideText3:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*600).fadeIn(700);
+});
+var $el = $(".slideText4:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*900).fadeIn(800);
+});
 //=======================AUDIO JS============================================================================
 
 
